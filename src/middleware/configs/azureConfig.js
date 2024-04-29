88c3ab4e007dbf8531bf0e6f6b57c066dc6d6445
Bhaskar Dutta <36147983+bhaskardutta95@users.noreply.github.com>
2024-04-29 17:43:55 +0530

    Create azureConfig.js

---
+++ src/middleware/configs/azureConfig.js
@@ -0,0 +1,8 @@
+const azureConfig = {
+    accountName : "stgarticle1",
+    accountKey : "",
+    containerName : "user-article",
+    blobName : "user-article"
+  };
+  
+module.exports = azureConfig;
