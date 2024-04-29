75c8c29a175c976129d2e499e2d7dcf27efcbfb0
Bhaskar Dutta <36147983+bhaskardutta95@users.noreply.github.com>
2024-04-29 17:40:59 +0530

    Create tagModel.js

---
+++ src/domain/models/tagModel.js
@@ -0,0 +1,19 @@
+const { DataTypes } = require("sequelize");
+const SqlConnection = require("../../middleware/connections/sqlConnection");
+const { setDateFormat, removeAttributeId } = require('../../middleware/configs/sequelizeConfig');
+const dbConstants = require('../../domain/constants/dbConstants');
+
+setDateFormat()
+const TagModel = SqlConnection.define('TAG', {
+    guid: DataTypes.STRING,
+    name: DataTypes.STRING,
+    createdDate: DataTypes.DATE,
+    modifiedDate: DataTypes.DATE
+}, {
+    timestamps: false,
+    freezeTableName: true,
+    tableName: dbConstants.TagTableName
+});
+removeAttributeId(TagModel)
+
+module.exports = TagModel
