0510bc29e8fa1222397c388ca33c49239ce022db
Bhaskar Dutta <36147983+bhaskardutta95@users.noreply.github.com>
2024-04-29 17:50:33 +0530

    Update index.js

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require( "express");
+const storageController = require( "./controller/azureStorageController");
+const tagController = require( "./controller/tagController");
+const { json, urlencoded } = require( 'body-parser');
+
+
+const app = express();
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use("/api", storageController);
+app.use("/api", tagController);
+
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log('Server status OK on port ' + port)
+})
